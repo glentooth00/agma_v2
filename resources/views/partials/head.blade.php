4fbe0a1dee97d1bcef1e6beeb3bf4189e960f34a
Glen <glendonnalpasan1@gmail.com>
2026-06-29 13:56:01 +0800

    Implement Membership functionality with controller, model, migration, and views

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,9 +5,9 @@
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="{{ asset('images/favicon_io/ileco32x32.png') }}" sizes="any">
+{{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
 
 @fonts
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\MembershipController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+
+    Route::get('/members', [MembershipController::class, 'index'])->name('members.index');
+
 });
 
 require __DIR__.'/settings.php';
